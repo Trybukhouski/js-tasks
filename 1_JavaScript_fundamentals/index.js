ea26bf1fc5f8eb23be799b82f1890d4121c34abe
Ruslan <trybukhouskij@gmail.com>
2022-02-08 23:17:42 +0300

    feat: implement tasks from topic Maths

--- 1_JavaScript_fundamentals/index.js
+++ 1_JavaScript_fundamentals/index.js
@@ -42,3 +42,68 @@ alert( `hello ${"name"}` ); // ?
 // hello name
 alert( `hello ${name}` ); // ?
 // hello Ilya
+
+
+
+
+// BASIC OPERATORS, MATHS
+
+// 1
+// What are the final values of all variables a, b, c and d after the code below?
+
+let a = 1, b = 1;
+let c = ++a;
+// Answer: 2
+let d = b++;
+// Answer: 1
+
+// 2
+// What are the values of a and x after the code below?
+
+let a = 2;
+let x = 1 + (a *= 2);
+// Answer: 5
+
+// 3
+// What are results of these expressions?
+
+"" + 1 + 0
+// Answer: 10
+"" - 1 + 0
+// Answer: -1
+true + false
+// Answer: 1
+6 / "3"
+// Answer: 2
+"2" * "3"
+// Answer: 6
+4 + 5 + "px"
+// Answer: 9px
+"$" + 4 + 5
+// Answer: $45
+"4" - 2
+// Answer: 2
+"4px" - 2
+// Answer: NaN
+"  -9  " + 5
+// Answer: stroke >> '   -9   5'
+"  -9  " - 5
+// Answer: -14
+null + 1
+// Answer: 1
+undefined + 1
+// Answer: NaN
+" \t \n" - 2
+// Answer: -2
+
+// 4
+// Here’s a code that asks the user for two numbers and shows their sum.
+// It works incorrectly. The output in the example below is 12 (for default prompt values).
+// Why? Fix it. The result should be 3.
+
+let a = prompt("First number?", 1);
+let b = prompt("Second number?", 2);
+
+alert(+a + +b);
+
+
