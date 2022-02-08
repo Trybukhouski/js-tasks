dfd5ec9a04be5e90f924e4b87dd7632a4e9d5dad
Ruslan <trybukhouskij@gmail.com>
2022-02-09 02:49:41 +0300

    feat: implement tasks from topic Switch

--- 1_JavaScript_fundamentals/index.js
+++ 1_JavaScript_fundamentals/index.js
@@ -312,3 +312,43 @@ for (let i = 2; i <= n; i++) {
   }
   alert(i);
 }
+
+
+
+
+// SWITCH
+
+// 1
+// https://javascript.info/task/rewrite-switch-if-else
+
+let browser = prompt('name', '');
+
+if (browser === 'Edge') {
+  alert( "You've got the Edge!" );
+} else if (browser === 'Chrome' || 
+  browser === 'Firefox' ||
+  browser === 'Safari' ||
+  browser === 'Opera') {
+  alert( 'Okay we support these browsers too' );
+} else {
+  alert( 'We hope that this page looks ok!' );
+}
+
+// 2
+// https://javascript.info/task/rewrite-if-switch
+
+let a = +prompt('a?', '');
+
+switch (a) {
+  case 0: 
+    alert(0);
+    break;
+  case 1: 
+    alert(1);
+    break;
+  case 2:
+  case 3: 
+    alert('2,3');
+    break;
+}
+
