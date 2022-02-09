3faecf4e30ba91c51b08d194424e9d84c8e042fc
Ruslan <trybukhouskij@gmail.com>
2022-02-09 14:48:38 +0300

    feat: add tasks from THIS

--- 2_Objects_the_basics/index.js
+++ 2_Objects_the_basics/index.js
@@ -49,3 +49,61 @@ function multiplyNumeric(obj) {
   return obj;
 }
 console.log(multiplyNumeric(menu));
+
+
+
+// THIS
+
+// https://javascript.info/task/object-property-this
+console.log(`-----Task 5-----`);
+
+function makeUser() {
+  return {
+    ref() {
+      return this;
+    },
+    name: "Джон",
+  };
+};
+let user1 = makeUser;
+console.log(user1().ref().name);
+
+// https://javascript.info/task/calculator
+console.log(`-----Task 6-----`);
+
+let calculator = {
+  read() {
+    this.firstValue = prompt('First Value', '');
+    this.secondtValue = prompt('Second Value', '');
+    return this;
+  },
+  sum() {
+    return +this.firstValue + +this.secondtValue;
+  },
+  mult() {
+    return this.firstValue * this.secondtValue;
+  },
+}
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mult() );
+
+// https://javascript.info/task/chain-calls
+console.log(`-----Task 6-----`);
+
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep() {
+    console.log( this.step );
+    return this;
+  }
+};
+ladder.up().up().down().showStep().down().showStep()
