df252ac28e76015ce1a3f07187611716dfebd5e8
Ruslan <trybukhouskij@gmail.com>
2022-02-11 15:15:30 +0300

    feat: add tasks from Data types chapter (part 2)

--- 3_Data-types/index.js
+++ 3_Data-types/index.js
@@ -125,3 +125,115 @@ function filterRange(arr, a, b) {
   return arr.filter(el => (el >= a) && (el <= b))
 }
 console.log(filterRange(array, 1, 4));
+
+
+console.log('-----Task 14-----');
+// https://learn.javascript.ru/task/filter-range-in-place
+
+function filterRangeInPlace(arr, a, b) {
+  arr = arr.filter(el => (el >= a) && (el <= b))
+  console.log(arr);
+}
+
+
+console.log('-----Task 15-----');
+// https://learn.javascript.ru/task/sort-back
+
+let arr = [5, 2, 1, -10, 8];
+console.log(arr.sort((a, b) => b - a));
+
+
+console.log('-----Task 16-----');
+// https://learn.javascript.ru/task/copy-sort-array
+
+let ar = ["HTML", "JavaScript", "CSS"];
+const copySorted = arr => arr.slice().sort();
+let sorted = copySorted(array);
+console.log(ar, sorted)
+
+console.log('-----Task 17-----');
+// https://learn.javascript.ru/task/array-get-names
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+let users = [ vasya, petya, masha ];
+const result = users.map(el => el.name);
+console.log(result);
+
+console.log('-----Task 18-----');
+// https://learn.javascript.ru/task/map-objects
+let vasya2 = { name: "Вася", surname: "Пупкин", id: 1 };
+let petya2 = { name: "Петя", surname: "Иванов", id: 2 };
+let masha2 = { name: "Маша", surname: "Петрова", id: 3 };
+let users2 = [ vasya2, petya2, masha2 ];
+let usersMapped = users2.map(el => ({fullname: `${el.name} ${el.surname}`, id: el.id}))
+console.log(usersMapped);
+
+
+console.log('-----Task 19-----');
+// https://learn.javascript.ru/task/sort-objects
+let vasya3 = { name: "Вася", age: 25 };
+let petya3 = { name: "Петя", age: 30 };
+let masha3 = { name: "Маша", age: 28 };
+let noSortArr = [ vasya3, petya3, masha3 ];
+function sortByAge(arr) {
+  return arr.sort((a, b) => a.name > b.name ? 1 : -1);
+}
+console.log(sortByAge(noSortArr));
+
+
+console.log('-----Task 20-----');
+// https://learn.javascript.ru/task/average-age
+
+let vasya4 = { name: "Вася", age: 25 };
+let petya4 = { name: "Петя", age: 30 };
+let masha4 = { name: "Маша", age: 29 };
+let arr4 = [ vasya4, petya4, masha4 ];
+const getAverageAge = arr => arr4.reduce((acc, el) => acc + el.age, 0) / arr.length;
+console.log(getAverageAge(arr4));
+
+
+console.log('-----Task 20-----');
+// https://learn.javascript.ru/task/array-unique
+let strings = ["кришна", "кришна", "харе", "харе",
+  "харе", "харе", "кришна", "кришна", ":-O"];
+function unique(arr) {
+  const result = [];
+  for(let i = 0; i < arr.length; i++) {
+    if(!result.includes(arr[i])) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+console.log(unique(strings));
+
+
+console.log('-----Task 21-----');
+// https://learn.javascript.ru/task/serialize-object
+
+let user = {
+  name: "Василий Иванович",
+  age: 35
+};
+let jsonUser = JSON.stringify(user);
+let noJsonUser = JSON.parse(jsonUser);
+console.log(jsonUser, noJsonUser);
+
+
+console.log('-----Task 22-----');
+// https://learn.javascript.ru/task/serialize-event-circular
+
+let room = {
+  number: 23
+};
+let meetup = {
+  title: "Совещание",
+  occupiedBy: [{name: "Иванов"}, {name: "Петров"}],
+  place: room
+};
+room.occupiedBy = meetup;
+meetup.self = meetup;
+let result2 = JSON.stringify(meetup, (key, value) => (value === meetup && key !== "") ? undefined : value)
+console.log(result2);
