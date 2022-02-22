4a707d7f898f4e919b7fcf2debcb6b58a39a7e5d
Ruslan <trybukhouskij@gmail.com>
2022-02-22 15:15:13 +0300

    feat: improve cacher

--- 6_Implement_function-cacher/index.js
+++ 6_Implement_function-cacher/index.js
@@ -1,28 +1,32 @@
-function factorial(num) {                           // Создаем факториал
-  return num !== 1 ? num * factorial(num - 1) : 1;
+function factorial(...nums) { 
+  return nums.map((num) => 
+    num !== 1 ? num * factorial(num - 1) : 1); 
 }
 
-class Cacher {                                       // Создаем класс с кеширующей функцией
-  withCache(func) {                                  // Создаем декоратор, проверяющий, находится ли данное в кеше
-    let cache = new Map();                           // Создаем коллекцию map, выполняющую роль кеша
-    return function(key) {                           // Создаем замыкание для сохранения данных в кеше
-      if(cache.has(key)) {                           // Проверяем, есть ли данное в кеше
-        return `${cache.get(key)} - из кеша`;        // Если есть, берем данное из кеша
-      } else {
-        let result = func(key);                      // Если нет, то выполняем функцию и сохраняем ее результат в кеше
-        cache.set(key, result);
+class Cacher {
+  withCache(func) {
+    let cache = new Map();
+    return function(...keys) {
+      let result = [];
+      for(let key of keys) {
+        if(cache.has(key)) {
+          result.push(`${cache.get(key)} - из кеша`); 
+        } else {
+          let value = +func(key);
+          cache.set(key, value);
+          result.push(value);
+        }       
       }
-      return cache.get(key);                         // Возвращаем сохраненный в кеше результат выполнения функции
+      return result;
     }
   }
 }
 
-const cacher = new Cacher();                        // Создаем экземпляр класса
-const cachedFactorial = cacher.withCache(factorial);// Создаем переменную и присваиваем ей метод кеширования из созданного ранее класса
-const a = cachedFactorial(5);                       // Передаем в метод число для расчета факториала
+const cacher = new Cacher();
+const cachedFactorial = cacher.withCache(factorial);
+const a = cachedFactorial(5, 4, 3);
 console.log(a);
-const b = cachedFactorial(5);                       // Передаем в метод аналоничное число и проверяем, взято ли оно из кеша
-console.log(b);
-const c = cachedFactorial(4);                       // Передаем другое число и проверяем резульат
+const b = cachedFactorial(5, 3);
+console.log(b)
+const c = cachedFactorial(3);
 console.log(c);
-
