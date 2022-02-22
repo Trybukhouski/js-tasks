ffe97591ac2d26b186aef6491a97dfef1050ea0e
Ruslan <trybukhouskij@gmail.com>
2022-02-22 17:24:59 +0300

    rewrite cacher

--- 6_Implement_function-cacher/index.js
+++ 6_Implement_function-cacher/index.js
@@ -1,32 +1,42 @@
-function factorial(...nums) { 
-  return nums.map((num) => 
-    num !== 1 ? num * factorial(num - 1) : 1); 
+function sum(a, b, c) {
+  return a + b + c;
 }
+
+function division(a, b) {
+  return a / b;
+}
+
 
 class Cacher {
   withCache(func) {
     let cache = new Map();
     return function(...keys) {
-      let result = [];
-      for(let key of keys) {
-        if(cache.has(key)) {
-          result.push(`${cache.get(key)} - из кеша`); 
-        } else {
-          let value = +func(key);
-          cache.set(key, value);
-          result.push(value);
-        }       
+      let args = JSON.stringify(arguments);
+      if(cache.has(args)) {
+        return `${cache.get(args)} cash`; 
+      } else {
+        let value = func(...keys);
+        cache.set(JSON.stringify(arguments), value);
+        return value;
       }
-      return result;
     }
   }
 }
 
 const cacher = new Cacher();
-const cachedFactorial = cacher.withCache(factorial);
-const a = cachedFactorial(5, 4, 3);
+
+const cachedSum = cacher.withCache(sum);
+const a = cachedSum(5, 4, 3);
 console.log(a);
-const b = cachedFactorial(5, 3);
+const b = cachedSum(5, 4, 3);
 console.log(b)
-const c = cachedFactorial(3);
+const c = cachedSum(3, 4, 6);
 console.log(c);
+
+const cachedDiv = cacher.withCache(division);
+const d = cachedDiv(5, 2);
+console.log(d);
+const e = cachedDiv(5, 2);
+console.log(e);
+const f = cachedDiv(10, 2);
+console.log(f);
