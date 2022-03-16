e105630dd1be6d6cc66d106d493016f8f8d22b35
Ruslan <trybukhouskij@gmail.com>
2022-03-16 14:30:43 +0300

    add solution for task ASYNC - part 2

--- 8_task-2_Async/script.mjs
+++ 8_task-2_Async/script.mjs
@@ -25,154 +25,88 @@ const addElement = (url) => {
   }, 100);
 }
 
+async function fetchImage() {
+  const result = await fetch("https://api.thecatapi.com/v1/images/search")
+      .then(res => res.json())
+      .then(data => data[0].url);
+  return result;
+}
+
 // Загрузка трех первых фото
 
 function addImage() {
-  fetch("https://api.thecatapi.com/v1/images/search")
-      .then(res => res.json())
-      .then(data => data[0].url)
-      .then(url => addElement(url));
+  fetchImage().then(url => addElement(url));
 }
 
 addImage();
 addImage();
 addImage();
 
+// CALLBACKS
 
+function addImagesCB(n, callback) {
+  for (let i = 0; i < n; i++) {
+    fetchImage().then((data) => callback(data));
+  }
+}
+
+function addAllImmagesCB(n, callback) {
+  const arr = promiseArrGenerator(n);
+  arr.forEach(el => el.then(url => callback(url)))
+}
+
+function addFirstImmageCB(n, callback) {
+  const arr = promiseArrGenerator(n);
+  arr[0].then(url => callback(url));
+}
+addFirstImmageCB(5, addElement)
 
 // PROMISE
 
-async function f() {
-
-  // Загрузка изображений одно за другим
-
-  await new Promise((res, rej) => {
-    fetch("https://api.thecatapi.com/v1/images/search")
-      .then(res => res.json())
-      .then(data => data[0].url)
-      .then(result => res(result));
-  })
-    .then((result) => {
-      addElement(result);
-      return new Promise((res, rej) => {
-        fetch("https://api.thecatapi.com/v1/images/search")
-          .then(res => res.json())
-          .then(data => data[0].url)
-          .then(result => res(result));
-      })
-    })
-    .then((result) => {
-      addElement(result);
-      return new Promise((res, rej) => {
-        fetch("https://api.thecatapi.com/v1/images/search")
-          .then(res => res.json())
-          .then(data => data[0].url)
-          .then(result => res(result));
-      })
-    })
-    .then((result) => {
-      addElement(result);
-      return new Promise((res, rej) => {
-        fetch("https://api.thecatapi.com/v1/images/search")
-          .then(res => res.json())
-          .then(data => data[0].url)
-          .then(result => res(result));
-      })
-    })
-    .then((result) => {
-      addElement(result);
-      return new Promise((res, rej) => {
-        fetch("https://api.thecatapi.com/v1/images/search")
-          .then(res => res.json())
-          .then(data => data[0].url)
-          .then(result => res(result));
-      })
-    })
-    .then((result) => {
-      addElement(result);
-    })
-
-  // Загрузка изображений одновременно
-
-  const firstImage = new Promise((res, rej) => {
-    fetch("https://api.thecatapi.com/v1/images/search")
-      .then(res => res.json())
-      .then(data => data[0].url)
-      .then(result => res(result));
-  })
-
-  const secondImage = new Promise((res, rej) => {
-    fetch("https://api.thecatapi.com/v1/images/search")
-      .then(res => res.json())
-      .then(data => data[0].url)
-      .then(result => res(result));
-  })
-
-  const thirdImage = new Promise((res, rej) => {
-    fetch("https://api.thecatapi.com/v1/images/search")
-      .then(res => res.json())
-      .then(data => data[0].url)
-      .then(result => res(result));
-  })
-
-  const fourthImage = new Promise((res, rej) => {
-    fetch("https://api.thecatapi.com/v1/images/search")
-      .then(res => res.json())
-      .then(data => data[0].url)
-      .then(result => res(result));
-  })
-
-  const fifthImage = new Promise((res, rej) => {
-    fetch("https://api.thecatapi.com/v1/images/search")
-      .then(res => res.json())
-      .then(data => data[0].url)
-      .then(result => res(result));
-  })
-
-  await Promise.all([firstImage, secondImage, thirdImage, fourthImage, fifthImage])
-    .then((value) => {
-      value.forEach(el => addElement(el))
-    })
-
-  // Загрузка первого изображения
-
-  const firstRaceImage = new Promise((res, rej) => {
-    fetch("https://api.thecatapi.com/v1/images/search")
-      .then(res => res.json())
-      .then(data => data[0].url)
-      .then(result => res(result));
-  })
-
-  const secondRaceImage = new Promise((res, rej) => {
-    fetch("https://api.thecatapi.com/v1/images/search")
-      .then(res => res.json())
-      .then(data => data[0].url)
-      .then(result => res(result));
-  })
-
-  const thirdRaceImage = new Promise((res, rej) => {
-    fetch("https://api.thecatapi.com/v1/images/search")
-      .then(res => res.json())
-      .then(data => data[0].url)
-      .then(result => res(result));
-  })
-
-  const fourthRaceImage = new Promise((res, rej) => {
-    fetch("https://api.thecatapi.com/v1/images/search")
-      .then(res => res.json())
-      .then(data => data[0].url)
-      .then(result => res(result));
-  })
-
-  const fifthRaceImage = new Promise((res, rej) => {
-    fetch("https://api.thecatapi.com/v1/images/search")
-      .then(res => res.json())
-      .then(data => data[0].url)
-      .then(result => res(result));
-  })
-
-  await Promise.race([firstRaceImage, secondRaceImage, thirdRaceImage, fourthRaceImage, fifthRaceImage])
-    .then((result => addElement(result)));
+const loading = n => {
+  if(n === 0) {
+    return fetchImage();
+  } else {
+    loading(n - 1).then(url => addElement(url));
+    return fetchImage();
+  }
 }
 
+function promiseArrGenerator(n) {
+  let arr = [];
+  let i = 1;
+  while(i <= n) {
+    arr.push(fetchImage())
+    i++;
+  }
+  return arr;
+}
+
+// Async await
+
+async function f() {
+
+  // CB: Загрузка изображений одно за другим
+  await addImagesCB(5, addElement);
+
+  // CB: Загрузка изображений одновременно
+  await addAllImmagesCB(5, addElement);
+
+  // Promise: Отображение первого изображения
+
+  await addFirstImmageCB(5, addElement);
+
+  // Promise: Загрузка изображений одно за другим
+  await loading(5);
+
+  // Promise: Загрузка изображений одновременно
+  await Promise.all(promiseArrGenerator(5))
+    .then((value) => {
+    value.forEach(el => addElement(el))
+  })
+
+  // Promise: Отображение первого изображения
+  await Promise.race(promiseArrGenerator(5))
+    .then((result => addElement(result)));
+}
 f();
